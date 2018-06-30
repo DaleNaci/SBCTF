bccf410ba2a6885b6f2fbc36a0bffa60957efaed
DaleNaci <dale.nash01@gmail.com>
2018-06-30 17:51:56 -0400

    Updated build with Firebase question

--- events.js
+++ events.js
@@ -1,8 +1,15 @@
+let initialized = false;
+
 window.addEventListener("_event_onInitializeFirebase", e => {
-    firebase.initializeApp(e.detail.configuration);
-    command();
+    if(!initialized) {
+        firebase.initializeApp(e.detail.configuration);
+        command();
+        initialized = true;
+    }
     firebase.auth().onAuthStateChanged(user => {
+        e.detail.saveUser(user);
         if(user) {
+            e.detail.onSignIn();
             window.dispatchEvent(new CustomEvent("signedIn"));
         }
         else {
@@ -16,6 +23,24 @@ window.addEventListener("_event_onSignIn", e => {
     promise.then(e.detail.then).catch(e.detail.catch);
 });
 
+window.addEventListener("_event_onSignUp", e => {
+    let promise = firebase.auth().createUserWithEmailAndPassword(e.detail.email, e.detail.password);
+    promise.then( () => {
+        e.detail.then();
+        firebase.database().ref("userinfo/"+firebase.auth().currentUser.uid).set({
+            name: e.detail.name,
+            answered: [],
+            points: 0
+        });
+    }).catch(e.detail.catch);
+});
+
 window.addEventListener("_event_onSignOut", e => {
     firebase.auth().signOut();
 });
+
+window.addEventListener("_event_onGetData", e => {
+    firebase.database().ref(e.detail.reference).once("value", snap => {
+        e.detail.callback(snap.val());
+    });
+})
