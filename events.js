366cb0dc892d8566d5e3fcd825f189d7e0c59d59
rishavb123 <rishavb123@gmail.com>
2018-07-03 16:47:27 -0400

    testing new npm committing message

---
+++ events.js
@@ -0,0 +1,46 @@
+let initialized = false;
+
+window.addEventListener("_event_onInitializeFirebase", e => {
+    if(!initialized) {
+        firebase.initializeApp(e.detail.configuration);
+        command();
+        initialized = true;
+    }
+    firebase.auth().onAuthStateChanged(user => {
+        e.detail.saveUser(user);
+        if(user) {
+            e.detail.onSignIn();
+            window.dispatchEvent(new CustomEvent("signedIn"));
+        }
+        else {
+            window.dispatchEvent(new CustomEvent("signedOut"));
+        }
+    });
+});
+
+window.addEventListener("_event_onSignIn", e => {
+    let promise = firebase.auth().signInWithEmailAndPassword(e.detail.email, e.detail.password);
+    promise.then(e.detail.then).catch(e.detail.catch);
+});
+
+window.addEventListener("_event_onSignUp", e => {
+    let promise = firebase.auth().createUserWithEmailAndPassword(e.detail.email, e.detail.password);
+    promise.then( () => {
+        e.detail.then();
+        firebase.database().ref("userinfo/"+firebase.auth().currentUser.uid).set({
+            name: e.detail.name,
+            answered: [],
+            points: 0
+        });
+    }).catch(e.detail.catch);
+});
+
+window.addEventListener("_event_onSignOut", e => {
+    firebase.auth().signOut();
+});
+
+window.addEventListener("_event_onGetData", e => {
+    firebase.database().ref(e.detail.reference).once("value", snap => {
+        e.detail.callback(snap.val());
+    });
+})
